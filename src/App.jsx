a7fb7d95fface4f7bde808897488a78627ecef86
kenkindermann <ken.kindermann@gmail.com>
2024-02-15 08:42:55 +0100

    add body style to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="page-content bg-gradient-to-b from-dark-blue to-stone-800 text-white font-roboto">
       <Navbar />
-    </>
+    </div>
   );
 }
 
