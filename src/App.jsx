46f48bd3ed5ca83b713c54f615366c544b07c1e0
kenkindermann <ken.kindermann@gmail.com>
2024-02-15 11:15:00 +0100

    add route for "/"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+// CSS
 import "./App.css";
+
+// Router
+import { Route, Routes } from "react-router-dom";
+
+// Components
 import Popup from "./components/global/Popup";
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/main/Hero";
 
 function App() {
   return (
     <div className="page-content bg-gradient-to-b from-dark-blue to-stone-800 text-white font-roboto">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+      </Routes>
       <Popup />
     </div>
   );
