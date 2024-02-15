e908b30e7b9d695dc9548965c435b11d4e9e2340
kenkindermann <ken.kindermann@gmail.com>
2024-02-15 09:26:40 +0100

    add comments

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,6 +19,7 @@ const Navbar = () => {
   return (
     <nav className="bg-slate-700 shadow-md shadow-slate-600 bg-opacity-20 w-full">
       <div className="navbar flex items-center justify-between max-w-screen-2xl mx-auto p-4 gap-4">
+        {/* Logo and Searchbar */}
         <div className={`flex shrink-[3] gap-8 w-full sm:w-3/4 lg:w-5/12 ${showSearch && `z-10`} relative text-black`}>
           <img
             src={logo}
@@ -30,6 +31,7 @@ const Navbar = () => {
           <Searchbar />
         </div>
 
+        {/* Nav links */}
         <NavbarLinks showMobileNav={showMobileNav} setShowMobileNav={setShowMobileNav} />
 
         <div
@@ -37,6 +39,7 @@ const Navbar = () => {
             showMobileNav ? `absolute top-3 left-1/2 -translate-x-1/2 flex gap-4 ` : `flex gap-4 hidden sm:flex`
           }
         >
+          {/* Wishlist */}
           <div className="my-auto relative w-6" onClick={() => openPopup("wishList")}>
             {wishList.length > 0 && <Badge />}
             <img
@@ -46,6 +49,7 @@ const Navbar = () => {
             />
           </div>
 
+          {/* Shopping cart */}
           <div className="my-auto relative w-6" onClick={() => openPopup("shoppingCart")}>
             {shoppingCart.length > 0 && <Badge />}
             <img
@@ -55,6 +59,7 @@ const Navbar = () => {
             />
           </div>
 
+          {/* Sign up */}
           <button className="btn whitespace-nowrap" onClick={() => openPopup("signIn")}>
             Sign up
           </button>
