1e96388c26a087b9e5c2019625eb338c68e288ca
kenkindermann <ken.kindermann@gmail.com>
2024-02-15 08:42:42 +0100

    add popupProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { ProductProvider, ProductContext } from "./provider/ProductContext.jsx";
+import { ProductProvider } from "./provider/ProductContext.jsx";
+import { PopupProvider } from "./provider/PopupContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <ProductProvider>
-        <App />
+        <PopupProvider>
+          <App />
+        </PopupProvider>
       </ProductProvider>
     </BrowserRouter>
   </React.StrictMode>
