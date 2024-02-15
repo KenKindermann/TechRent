ec65be927061160ba3e5ff9d59050af4e2eac53e
kenkindermann <ken.kindermann@gmail.com>
2024-02-15 19:23:12 +0100

    add benefits data

---
+++ src/data/BenefitsData.js
@@ -0,0 +1,25 @@
+const BenefitsData = [
+  {
+    header: "Flexibility",
+    header2: "Rent for as long as you desire",
+    description:
+      "You have the power to decide the duration of your rental. After the minimum rental period, you have the option to return the device or continue to keep it.",
+    img: "/src/assets/img/applewatchultra2.png",
+  },
+  {
+    header: "Customizable",
+    header2: "Rental Period Rent on your own terms",
+    description:
+      "You determine the length of your rental. You can send back the device after the minimum rental period or choose to keep it for a longer duration.",
+    img: "/src/assets/img/applewatchultra2.png",
+  },
+  {
+    header: "Warranty",
+    header2: "Throughout Rental Rent with peace of mind",
+    description:
+      "Enjoy the assurance of a warranty for the entire duration of your rental. If anything goes wrong, you're covered.",
+    img: "/src/assets/img/applewatchultra2.png",
+  },
+];
+
+export default BenefitsData;
