88951fd251a7c7ac42309356cd772353644d7f0b
kenkindermann <ken.kindermann@gmail.com>
2024-02-16 20:04:05 +0100

    implement delivery section in productpage

---
+++ src/components/product-page/Delivery.jsx
@@ -0,0 +1,10 @@
+const Delivery = () => {
+  return (
+    <div>
+      <h3 className="text-xl mt-6">Delivery</h3>
+      <p>Delivery within 1-3 working days. Free shipping.</p>
+    </div>
+  );
+};
+
+export default Delivery;
