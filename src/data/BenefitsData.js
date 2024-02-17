1ba6d885d86aef5bdf237370a9a6dd36c808855a
kenkindermann <ken.kindermann@gmail.com>
2024-02-17 09:17:28 +0100

    add benefits images

--- src/data/BenefitsData.js
+++ src/data/BenefitsData.js
@@ -1,24 +1,29 @@
+// Images
+import infinity from "../assets/images/infinity.png";
+import flexibility from "../assets/images/flexibility.png";
+import security from "../assets/images/security.png";
+
 const BenefitsData = [
   {
     header: "Flexibility",
     header2: "Rent for as long as you desire",
     description:
       "You have the power to decide the duration of your rental. After the minimum rental period, you have the option to return the device or continue to keep it.",
-    img: "/src/assets/img/applewatchultra2.png",
+    img: infinity,
   },
   {
     header: "Customizable",
     header2: "Rental Period Rent on your own terms",
     description:
       "You determine the length of your rental. You can send back the device after the minimum rental period or choose to keep it for a longer duration.",
-    img: "/src/assets/img/applewatchultra2.png",
+    img: flexibility,
   },
   {
     header: "Warranty",
     header2: "Throughout Rental Rent with peace of mind",
     description:
       "Enjoy the assurance of a warranty for the entire duration of your rental. If anything goes wrong, you're covered.",
-    img: "/src/assets/img/applewatchultra2.png",
+    img: security,
   },
 ];
 
